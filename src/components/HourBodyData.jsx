0ee043e4422274374e5c901d95ccec31736497b0
David Abell <david.abell.m@gmail.com>
2022-06-24 14:10:05 +0100

    fix: rainfall percent, add: rainfall mm

--- src/components/HourBodyData.jsx
+++ src/components/HourBodyData.jsx
@@ -4,26 +4,39 @@ import WeatherSymbol from "./WeatherSymbol";
 function HourBodyData({ hour, hourData }) {
   const temperature = hourData[0].location.temperature.value;
   const symbolName = hourData[1].location.symbol.id;
-  const precipChance = hourData[1].location.precipitation.value;
+  let precipAmount;
+  const minPrecip = hourData[1].location.precipitation.minvalue;
+  const maxPrecip = hourData[1].location.precipitation.maxvalue;
+  if (minPrecip && maxPrecip) {
+    precipAmount = (
+      (parseFloat(minPrecip) + parseFloat(maxPrecip)) /
+      2
+    ).toFixed(2);
+  } else {
+    precipAmount = hourData[1].location.precipitation.value;
+  }
+  const precipChance = Math.round(
+    hourData[1].location.precipitation.probability
+  );
   const windDirection = hourData[0].location.windDirection.name;
   const windDegree = hourData[0].location.windDirection.deg;
   const windSpeed = hourData[0].location.windSpeed.mps;
   return (
     <Row className="align-items-center hour-row">
-      <Col xl={6} sm={12} lg={6} md={5}>
+      <Col sm={12} md={4}>
         <Row className="align-items-center">
           <Col className="d-flex justify-content-center">
-            <p className="my-0">{hour}</p>
+            <b className="my-0">{hour}</b>
           </Col>
           <Col className="d-flex justify-content-center">
-            <p className="my-0">{temperature} &deg;C</p>
+            <b className="my-0">{temperature} &deg;C</b>
           </Col>
           <Col className="d-flex justify-content-center">
             <WeatherSymbol symbolName={symbolName} />
           </Col>
         </Row>
       </Col>
-      <Col sm={12} xl={4} lg={3} md={4} className="text-center">
+      <Col sm={12} md={4} className="text-center">
         <Row className="align-items-center">
           <Col className="d-flex justify-content-center">
             <WeatherSymbol symbolName={"Wind"} rotateDegree={windDegree - 90} />
@@ -36,13 +49,19 @@ function HourBodyData({ hour, hourData }) {
           </Col>
         </Row>
       </Col>
-      <Col className="text-center" sm={12} xl={2} lg={2} md={3}>
+      <Col className="text-center" sm={12} md={4}>
         <Row className="align-items-center justify-content-center">
           <Col className="d-flex justify-content-center">
             <WeatherSymbol symbolName={"Raindrops"} />
           </Col>
           <Col className="d-flex justify-content-center">
-            <b className="my-0">{precipChance * 100 + "%"}</b>
+            <b className="my-0">{precipAmount + " mm"}</b>
+          </Col>
+
+          <Col className="d-flex justify-content-center">
+            <b className="my-0">
+              {precipChance || precipChance === 0 ? precipChance + "%" : ""}
+            </b>
           </Col>
         </Row>
       </Col>
