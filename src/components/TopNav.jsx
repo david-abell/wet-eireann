fd1e3cf7a2faca25881c423d76434ad39165052d
David Abell <david.abell.m@gmail.com>
2022-06-29 15:16:58 +0100

    fix: layout styles

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,26 +1,30 @@
 import { Navbar, Nav, Container, Form, Button } from "react-bootstrap";
 import MapSearch from "./MapSearch";
+import "../styles/topNav.css";
 function TopNav({ geoLocation, setGeoLocation }) {
   return (
     <Navbar expand="md" bg="light">
       <Container className="d-flex justify-content-between gap-2">
-        <Navbar.Brand href="#home" className="ms-2">
+        <Navbar.Brand href="#home" className="ms-md-4 flex-basis-50">
           Wet-Eireann
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav" className="ms-auto">
-          <Nav>
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#daily-forecast">Daily forecast</Nav.Link>
-            <Nav.Link href="#weather-graph">Weather Graph</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
         {geoLocation && (
           <MapSearch
             geoLocation={geoLocation}
             setGeoLocation={setGeoLocation}
           />
         )}
+        <Navbar.Collapse
+          id="navbar-nav"
+          className="m-auto ms-md-auto flex-shrink-0"
+        >
+          <Nav>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#daily-forecast">Daily forecast</Nav.Link>
+            <Nav.Link href="#weather-graph">Weather Graph</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
         {/* <Form className="ms-auto d-flex flex-grow-1">
           <Form.Group
             controlId="locationSearch"
